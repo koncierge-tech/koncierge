bb28b273ddb6746f1c637baded16b60a64a8e256
Evert <evert.hoff@gmail.com>
2023-11-11 15:39:58 +0200

    Parsing existing YAML files

---
+++ core/src/main/java/tech/koncierge/k8s/grammar/KubernetesOpenApi.java
@@ -0,0 +1,67 @@
+/*
+Copyright 2023 The Koncierge Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package tech.koncierge.k8s.grammar;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+public class KubernetesOpenApi {
+
+    private static OpenAPI openAPI;
+
+    public static OpenAPI getOpenAPI() {
+        if (openAPI == null) {
+            openAPI = parse();
+        }
+        return openAPI;
+    }
+
+    private static OpenAPI parse() {
+        OpenAPIV3Parser parser = new OpenAPIV3Parser();
+        OpenAPI openAPI = parser.read("./src/main/resources/kubernetes/k8s-swagger.json");
+        return openAPI;
+    }
+
+    public static Schema getSchema(String key) {
+        return getOpenAPI().getComponents().getSchemas().get(key);
+    }
+
+    public static String findSchemaId(String kind) {
+        for (String key : getOpenAPI().getComponents().getSchemas().keySet()) {
+            String partAfterLastDot = key.substring(key.lastIndexOf(".") + 1);
+            if (partAfterLastDot.equals(kind)) {
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public static String getSchemaNameFromRef(String ref) {
+        String key = ref.substring(ref.lastIndexOf("/") + 1);
+        return key;
+    }
+
+    public static Schema getSchemaFromRef(String ref) {
+        String key = ref.substring(ref.lastIndexOf("/") + 1);
+        return getSchema(key);
+    }
+
+    public static void main(String[] args) {
+        OpenAPI openAPI = parse();
+        System.out.println(openAPI);
+    }
+}
+
---
+++ core/src/test/java/tech/koncierge/k8s/parser/KubernetesManifestParserTest.java
@@ -0,0 +1,42 @@
+/*
+Copyright 2023 The Koncierge Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package tech.koncierge.k8s.parser;
+
+import org.junit.jupiter.api.Test;
+import tech.koncierge.model.kubernetes.Specification;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KubernetesManifestParserTest {
+
+    @Test
+    public void test() {
+        InputStream inputStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("kubernetes/example-manifest.yaml");
+        List<Specification> specifications = new KubernetesManifestParser().parseSpecification(inputStream);
+        assertEquals(2, specifications.size());
+
+        Specification specification1 = specifications.get(0);
+        assertEquals("io.k8s.api.apps.v1.Deployment", specification1.getSchemaId());
+
+        Specification specification2 = specifications.get(1);
+        assertEquals("io.k8s.api.core.v1.Service", specification2.getSchemaId());
+    }
+}
